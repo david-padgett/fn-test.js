45961d8c9d9693b72fe630c91775f62920688e89
David Padgett <david.padgett@icloud.com>
2016-03-13 20:17:25 -0700

    Changed behavior of error() API, added test cases.

--- src/test/javascript/node-suffix.js
+++ src/test/javascript/node-suffix.js
@@ -1,1 +1,3 @@
 // fn-test.js/src/test/javascript/node-suffix.js
+
+process.exit($test.getResult() ? 0 : -1);
--- src/test/javascript/test.js
+++ src/test/javascript/test.js
@@ -2,11 +2,16 @@
 
 console.log("")
 var $test = __FnTest;
-$test.expect(2, 2, 1);
+$test.expect(2, 2);
 $test.message("fn-test.html", "test of message API");
-$test.error(new Error("test of error API, used to test known error conditions"));
-$test.assert("test of assertion API - positive assertion", function() {return true;});
-$test.assert("test of assertion API - negative assertion", function() {return false;});
-$test.assert("test of assertion API - exception assertion", function() {throw new Error("uncaught exception assertion error");});
-$test.assert("test of assertion API - positive assertion", function() {return true;});
+$test.assert("test of assert API - positive test 1", function() {return true;});
+$test.assert("test of assert API - negative test 1", function() {return false;});
+$test.error("test of error API - positive test 1", function() {throw new Error("uncaught exception assertion error");});
+$test.error("test of error API - positive test 2", function() {throw new Error("uncaught exception assertion error");}, new Error("uncaught exception assertion error"));
+$test.error("test of error API - negative test 1", function() {throw {message: "non-Error object"};}, new Error("uncaught exception assertion error"));
+$test.error("test of error API - negative test 2", function() {throw new TypeError("incorrect error type");}, new Error("uncaught exception assertion error"));
+$test.error("test of error API - negative test 3", function() {return true;});
+$test.error("test of error API - negative test 4", function() {throw new Error("incorrect message");}, new Error("uncaught exception assertion error"));
+$test.assert("test of assert API - positive test 2", function() {return true;});
+
 $test.summary();
